660d7b79a23b8ed36533f9737d5adbb7a5e0018a
harshpamar19990810-hash <harsh.pamar19990810@gmail.com>
2025-12-12 08:49:45 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,2 @@
+# note-counter
+Indian Note Counter is a simple web app to calculate the total value of Indian currency notes. Enter the number of notes for each denomination (₹500, ₹200, etc.), and it instantly shows the total. Built using HTML, CSS, and JavaScript. Useful for shops, offices, and personal use.
---
+++ service-worker.js
@@ -0,0 +1,41 @@
+const cacheName = 'note-calculator-v1';
+const filesToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icon-192.png',
+  '/icon-512.png',
+  'https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css',
+  'https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap'
+];
+
+// Install
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+// Activate
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => {
+          if (key !== cacheName) return caches.delete(key);
+        })
+      )
+    )
+  );
+});
+
+// Fetch
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(response => {
+      return response || fetch(e.request);
+    })
+  );
+});
